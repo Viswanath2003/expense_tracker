2ec5228370d9c55c362498b93c278a1d66211c0e
Shanmukha Viswanath <rcharan3003@gmail.com>
2025-10-27 16:21:55 +0530

    new folder structure

--- backend/config.py
+++ backend/config.py
@@ -6,4 +6,4 @@ class Config:
     MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "root")
     MYSQL_DB = os.getenv("MYSQL_DB", "expense_tracker")
     MYSQL_CURSORCLASS = 'DictCursor'
-    MYSQL_CLIENT_FLAGS = [2048]
+    #MYSQL_CLIENT_FLAGS = [2048]
